a094c8381b00a519ab026705f5c9bd9a6a4c9202
shakhawath <m.shakhawath18@gmail.com>
2022-03-23 19:19:11 +0600

    added product to local storage

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
+import './Product.css';
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { name, price, id } = product;
+    const addToCart = (id) => {
+        addToDb(id);
+    }
     return (
-        <div>
-
+        <div className='product'>
+            <h3>{name}</h3>
+            <p>Price: {price}</p>
+            <p>Id: {id}</p>
+            <button onClick={() => addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
     return (
         <div>
-
+            <h1>Wellcome to Cosmetics store</h1>
+            {
+                products.map(product => <Product key={product.id} product={product}></Product>)
+            }
         </div>
     );
 };
